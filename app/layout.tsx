c851a0597f4ee9d74a7a4786d45498ee1f2d05a3
noapark94-blip <noapark94@gmail.com>
2026-08-16 09:52:21 +0900

    Load teacher class home polish styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./backup.css";
 import "./bulk-import.css";
 import "./bulk-account.css";
 import "./continuation-polish.css";
+import "./class-home-polish.css";
 
 export const metadata: Metadata = {
   title: "한살매 입시전문학원",
