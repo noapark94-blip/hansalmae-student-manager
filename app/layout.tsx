2d39a29ff8eef0fdbd9437b2a8b26f9041338836
noapark94-blip <noapark94@gmail.com>
2026-08-20 11:27:38 +0900

    Load correction history filter polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ import "./correction-management.css";
 import "./correction-report.css";
 import "./correction-management-override.css";
 import "./correction-calendar-polish.css";
+import "./correction-history-filter-polish.css";
 import "./weekly-timetable-compact.css";
 import "./student-page-polish.css";
 import "./class-modal-repair.css";
