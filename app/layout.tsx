45ac681b6b77d90fb476a265d4487714f2510952
noapark94-blip <noapark94@gmail.com>
2026-08-20 15:48:08 +0900

    Load fitted student learning history styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ import "./class-modal-repair.css";
 import "./correction-timetable-names.css";
 import "./correction-direct-badges.css";
 import "./student-learning-history.css";
+import "./student-learning-history-fit.css";
 import { EscapeModalCloser } from "./escape-modal-closer";
 import { ClassEditorPermanentDelete } from "./class-editor-permanent-delete";
 import { CorrectionSubjectTabs } from "./correction-subject-tabs";
