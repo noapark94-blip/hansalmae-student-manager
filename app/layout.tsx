588326a747474e9c7405391120a4849aa51e8354
noapark94-blip <noapark94@gmail.com>
2026-08-23 17:08:12 +0900

    계정 역할 설정 화면 UI 정돈

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,7 @@ import "./student-learning-history-fit.css";
 import "./class-common-record-compact.css";
 import "./class-record-compact.css";
 import "./invite-signup.css";
+import "./account-settings-polish.css";
 import { EscapeModalCloser } from "./escape-modal-closer";
 import { ClassEditorPermanentDelete } from "./class-editor-permanent-delete";
 import { CorrectionSubjectTabs } from "./correction-subject-tabs";
