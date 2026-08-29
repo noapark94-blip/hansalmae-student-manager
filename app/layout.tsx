ecf2ba0fe87f428e71a10a7c3f75fa3ce8ba3c7d
noapark94-blip <noapark94@gmail.com>
2026-08-29 21:45:06 +0900

    style: load desktop class workspace polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,7 @@ import "./student-delete-confirm-polish.css";
 import "./lesson-roster-override.css";
 import "./schedule-conflict-alert.css";
 import "./student-detail-desktop-readability.css";
+import "./class-workspace-desktop-polish.css";
 import { EscapeModalCloser } from "./escape-modal-closer";
 import { ClassEditorPermanentDelete } from "./class-editor-permanent-delete";
 import { AppDialogHost } from "./app-dialog";
